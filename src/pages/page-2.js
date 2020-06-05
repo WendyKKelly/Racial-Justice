39c587b3030a2bab89c82e703cfc348253b91144
Wendy Kelly <wendykelly@Wendys-iMac.local>
2020-06-04 18:40:12 -0700

    moved text around and added placeholder for video.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,52 +10,33 @@ const SecondPage = () => (
     <h1>People for Racial Justice - Invitation to Show Your Solidarity</h1>
     <p>
 
+    Dear [___]
 
-Dear _______________
+    Around the world, people are united in grief and outrage at the murder of George Floyd by Minneapolis police on May 25. Sadly, his death is one of too many in a long line of appalling acts of public racism and police brutality towards Black, Brown, Indigenous, Asian and other people of colour both in the U.S. and here in Canada.
 
-Around the world, people are united in grief and outrage at the death of Minneapolis resident George Floyd, who was murdered by police on May 25 while bystanders watched in horror. Sadly, his death is the only the most prominent recent death in a long continuous line of egregious acts of public racism and police brutality towards Black, Brown, Indigenous and other people of colour both in the U.S. and here in Canada.
+    Locally, many of us have come together with strangers, friends and family to express our shock, condemnation, righteous anger, pain and deep compassion for those who have been targeted and those who live in fear for themselves, their loved ones, their children. This moment of heightened attention gives us the opportunity to act with resolve, to stand up and speak out against anti-Black racism in particular, and racism and exclusion more generally.
 
-Locally, many of us have come together with strangers, friends and family to express our shock, condemnation, righteous anger, pain and deep compassion for those who have been targeted and those who live in fear for themselves, their loved ones, their children. This moment of heightened attention gives us the opportunity to act with resolve, to stand up and speak out against anti-Black racism in particular, and racism and exclusion more generally.
+    Feeling compelled to take action, a small group of Nelsonites has initiated an act of solidarity for social change. We are writing to invite you to take part in it by expressing your commitment to racial justice by way of a short share-able response to two questions:
 
-Feeling compelled to take action, a small group of Nelsonites has initiated an act of solidarity for social change. We are writing to invite you to take part in it by expressing your feelings about this moment and your commitment to racial justice by way of a short (1 minute) self-recorded video response to two questions:
+    1) What do you think needs to change to move our community towards racial equality and justice?
 
-1) What do you think needs to change to move our community towards racial equality and justice?
-2) (For white folks in particular*) What do you commit to doing to be part of the solution?
+    2) Have you experienced racism here that you feel comfortable sharing? (Please describe.)
+    OR, If you have not experienced racism, what is one thing you will commit to doing to increase racial justice here?
 
-*While we feel that it is of utmost important that white people do the work to call out racism and unlearn it collectively, we also encourage people of colour who feel comfortable sharing a short story about racism they have experienced here to substitute that story in place of the second question.
+    The goal is to exert pressure and build commitment to action. We plan to share this widely within the community. We all have a part to play, particularly those of us who are in positions of authority and bear the greatest responsibility to ensure racial justice.
 
-We are beginning by contacting people who we feel are leaders in our community, those we expect are already on board, and those who are both most vulnerable and most knowledgeable about the problem and the steps that will be necessary to bring about lasting change. As the collection of voices grows, we will share the videos with those who have yet to come on board. The goal is not to offer an opportunity for self-congratulation or the performance of virtue, but rather to exert pressure and build commitment to action, particularly on the part of those in power in our community who may not have previously considered the part they might play in bringing about greater racial justice.
+    The technology we are using is VideoAsk. You can access it here. [Detailed instructions at link.] Please choose either a short self-recorded video, audio, or text-based response. Check out our growing collection at [Video Ask site].
 
-This invitation to participate is open to everyone and we encourage you to share it with anyone you feel might like to participate. For those who may feel uncomfortable with the video format, we also welcome still photos – e.g., of people with their words of commitment on a sign held in front of them.
+    This invitation to participate is open to everyone. We encourage you to share it broadly. If you have any questions about this action, please ask.
 
-We are collating these short videos (and photos) in a format that we plan to make public [on Facebook? Or a website?], and to share with other organizations working for long-term change.  We also hope it will be welcomed as a tool to further inform our elected representatives, and all those in whom our communities place their trust within the institutions of power (e.g., Nelson Police Department, other Emergency Services, School Districts, Hospitals etc.)
+    Thank you for considering our invitation. Your voice matters. And thank you for caring.
 
-The technology we are using is [Flipgrid?]. You can access it here. [Detailed instructions at link.]
-We are aiming to start collating these by [DATE] and to begin sharing as soon as possible.
+    Sincerely,
 
-If you have any questions about this action, please don’t hesitate to ask. You can contact me at [fill in the blank].
-
-Thank you for considering our invitation. Your voice matters.
-And thank you for caring.
-
-
-Sincerely,
-
-
-
-Wendy Brown Kelly
-
-
-
-Shelina Musaji
-
-
-
-Madelyn MacKay
-
-
-
-Jennie Barron
+    Wendy Brown Kelly - wendykkelly@gmail.com
+    Shelina Musaji – shelinamusaji@hotmail.com
+    Madelyn MacKay – madelynbmackay@gmail.com
+    Jennie Barron – jenniebarron@netidea.com
 
 
 
